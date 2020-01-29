8a9c2ee355690da606ec0a08d4fb921c458eb03a
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-29 03:47:44 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,8 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
    
-    create_table :add_grade_and_birthdate_to_students do |t|
-      t.string :name
+    add_column :add_grade_and_birthdate_to_students do |t|
+      t.string :
      
     end
   end
